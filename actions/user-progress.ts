504a387ac2ac473dd1dc4b0aa41a6716e91c1d18
Anurag Singh <96870454+AnuragSingh2511@users.noreply.github.com>
2024-08-31 14:48:59 +0530

    add: shop layout

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -1,5 +1,6 @@
 "use server";
 
+
 import db from "@/db/drizzle";
 import { getCourseById, getUserProgress } from "@/db/queries";
 import { challengeProgress, challenges, userProgress } from "@/db/schema";
@@ -9,7 +10,7 @@ import { get } from "http";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-
+const POINTS_TO_REFILL = 10;
 export const upsertUserProgress = async (courseId: number) => {
 
     const { userId } = await auth();
@@ -106,4 +107,31 @@ export const reduceHearts = async (challengeId: number) => {
   revalidatePath("/quests");
   revalidatePath("/leaderboard");
   revalidatePath(`/lesson/${lessonId}`);
+}
+
+export const refillHearts = async () => {
+  const currentUserProgress = await getUserProgress(); 
+
+  if(!currentUserProgress){
+    throw new Error("User progress not found");
+  }
+
+  if(currentUserProgress.hearts === 5) {
+    throw new Error("Already full");
+  }
+
+  if(currentUserProgress.points < POINTS_TO_REFILL) {
+    throw new Error("Not enough points");
+  }
+
+  await db.update(userProgress).set({
+    hearts: 5,
+    points: currentUserProgress.points - POINTS_TO_REFILL,
+  }).where(eq(userProgress.userId, currentUserProgress.userId));
+
+  revalidatePath("/shop");
+  revalidatePath("/learn");
+  revalidatePath("/quests");
+  revalidatePath("/leaderboard");
+ 
 }
